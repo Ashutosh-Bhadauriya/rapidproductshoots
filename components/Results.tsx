05e22b0031deda91421eecf65e7dbf44db4418a9
varun-balani <varunprahladb@gmail.com>
2023-10-30 00:35:55 +0530

    make results page responsive

--- components/Results.tsx
+++ components/Results.tsx
@@ -3,32 +3,61 @@ import ResultImage from "./ResultImage";
 
 export default function Results({ images }: any) {
     return (
-        <div className="grid grid-cols-3 grid-rows-2 text-xl font-bold tracking-normal text-slate-900 sm:text-3xl gap-8">
-            <ResultImage src={images[5]} alt="Original product photo" />
-            <ResultImage
-                src={images[1]}
-                alt="Generated product shoot"
-                generated={true}
-            />
-            <ResultImage
-                src={images[2]}
-                alt="Generated product shoot"
-                generated={true}
-            />
-            <ResultImage
-                src={images[0]}
-                alt="Product photo without background"
-            />
-            <ResultImage
-                src={images[3]}
-                alt="Generated product shoot"
-                generated={true}
-            />
-            <ResultImage
-                src={images[4]}
-                alt="Generated product shoot"
-                generated={true}
-            />
-        </div>
+        <>
+            <div className="hidden invisible md:visible md:grid grid-cols-3 grid-rows-2 text-xl font-bold tracking-normal text-slate-900 sm:text-3xl gap-8">
+                <ResultImage src={images[5]} alt="Original product photo" />
+                <ResultImage
+                    src={images[1]}
+                    alt="Generated product shoot"
+                    generated={true}
+                />
+                <ResultImage
+                    src={images[2]}
+                    alt="Generated product shoot"
+                    generated={true}
+                />
+                <ResultImage
+                    src={images[0]}
+                    alt="Product photo without background"
+                />
+                <ResultImage
+                    src={images[3]}
+                    alt="Generated product shoot"
+                    generated={true}
+                />
+                <ResultImage
+                    src={images[4]}
+                    alt="Generated product shoot"
+                    generated={true}
+                />
+            </div>
+            <div className="md:hidden md:invisible flex flex-col text-xl font-bold tracking-normal text-slate-900 sm:text-3xl">
+                <ResultImage src={images[5]} alt="Original product photo" />
+                <ResultImage
+                    src={images[0]}
+                    alt="Product photo without background"
+                />
+                <ResultImage
+                    src={images[1]}
+                    alt="Generated product shoot"
+                    generated={true}
+                />
+                <ResultImage
+                    src={images[2]}
+                    alt="Generated product shoot"
+                    generated={true}
+                />
+                <ResultImage
+                    src={images[3]}
+                    alt="Generated product shoot"
+                    generated={true}
+                />
+                <ResultImage
+                    src={images[4]}
+                    alt="Generated product shoot"
+                    generated={true}
+                />
+            </div>
+        </>
     );
 }
